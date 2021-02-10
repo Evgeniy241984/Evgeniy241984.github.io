acd149b359f8d48a747b6e880237e01d76471cf5
Evgen241984 <evgeniy241984@gmail.com>
2021-02-10 17:14:32 +0200

    friends app bugs fixed

--- friends_app/app.js
+++ friends_app/app.js
@@ -1,28 +1,59 @@
-const REQUEST_URL  = 'https://randomuser.me/api/?results=20',
+const REQUEST_URL = 'https://randomuser.me/api/?results=20',
+    HEADER = document.querySelector('.header-name'),
     FRIENDS_LIST_WRAP = document.querySelector('.friendsList-wrap'),
     MENU_WRAP = document.querySelector('.menu-wrap'),
-    SORTING_MENU = document.querySelector('.filter-sort-menu'),
     SEARCH_FIELD = document.querySelector('#search'),
-    SORT_BY_NAME_AZ = document.querySelector('.nameSortAZ'),
-    SORT_BY_NAME_ZA = document.querySelector('.nameSortZA'),
-    SORT_BY_AGE_YO = document.querySelector('.ageSortYoungerFirst'),
-    SORT_BY_AGE_OY = document.querySelector('.ageSortOlderFirst'),
-    FILTER_MENU = document.querySelector('.filter-gender'),
-    FILTER_BY_MALE = document.querySelector('.filter-gender__male'),
-    FILTER_BY_FEMALE = document.querySelector('.filter-gender__female'),
-    FILTER_BY_ALL = document.querySelector('.filter-gender__all'),
+    SORTING_MENU = document.querySelector('.sorting-menu'),
+    FILTER_MENU = document.querySelector('.filter-menu'),
     MOBILE_MENU_BUTTON = document.querySelector('.button-container');
-let friendsList = [],  
+let friendsList = [],
     searchResultList,
     sortedList,
     errorButton,
     genderNeutralList, filteredByGenderList,
-    scrollDepth;
-    
-const sendRequest = function(method, url) {
+    scrollDepth,
+    lettersArr = HEADER.innerHTML.split('');
+
+//convert int to hex
+const colToHex = function (c) {
+    //colors are bright enough?
+    let color = (c < 75) ? c + 75 : c
+    let hex = color.toString(16);
+    return hex.length == 1 ? '0' + hex : hex;
+}
+
+// colToHex to concatenate a full 6 digit hex code
+const rgbToHex = function (r, g, b) {
+    return '#' + colToHex(r) + colToHex(g) + colToHex(b);
+}
+
+const getRandomColor = function () {
+    return rgbToHex(
+        Math.floor(Math.random() * 255),
+        Math.floor(Math.random() * 255),
+        Math.floor(Math.random() * 255)
+    )
+}
+
+const randomColor = function () {
+    let html = '';
+    lettersArr.map(function (letter) {
+        let color = getRandomColor();
+        return html +=
+            `<span style= color:${color}>
+          ${letter}
+          </span>`
+    })
+    return html;
+}
+
+HEADER.innerHTML = randomColor();
+
+
+const sendRequest = function (url) {
     return fetch(url)
         .then(handleErrors)
-        .then(function(response) {
+        .then(function (response) {
             return response.json();
         });
 }
@@ -35,172 +66,157 @@ function handleErrors(response) {
 }
 
 function startApp() {
-    sendRequest('GET', REQUEST_URL)
-    .then( function(data) { 
-        friendsList = data.results;
-        renderFriendsList(friendsList);
-    })
-    .catch( function(err) {
-        renderErrorMessage();
-    });
+    sendRequest(REQUEST_URL)
+        .then(function (data) {
+            friendsList = data.results;
+            renderFriendsList(friendsList);
+        })
+        .catch(function (err) {
+            renderErrorMessage();
+        });
 }
 
 startApp();
 
-// drawing a list of friends---------------------------
 function renderFriendsList(friends) {
     FRIENDS_LIST_WRAP.innerHTML = '';
-    
-    friends.forEach(function(friend) {
+
+    friends.forEach(function (friend) {
         renderFriendsCard(friend);
     });
 }
 
-// create friends card, add card to the markup-----------
 function renderFriendsCard(friend) {
-    const cardWrap = document.createElement('div');
-    const mainInfo = document.createElement('div');
-    const friendsName = document.createElement('a');
-    const moreWrap = document.createElement('div');
-    const friendPhotoWrap = document.createElement('div')
-    const friendPhoto = document.createElement('img');
-    const friendAge = document.createElement('p');
-    const friendEmail = document.createElement('a');
-    const friendPhone = document.createElement('a');
-    const friendCity = document.createElement('a');
-
-    cardWrap.classList.add('cardWrap');
-    (friend.gender === 'male') ? cardWrap.classList.add('malegradient') : cardWrap.classList.add('femalegradient') 
-    mainInfo.classList.add('mainInfo');
-    moreWrap.classList.add('moreWrap');
-    friendPhotoWrap.classList.add('cardWrap__photo-wrap');
-    friendPhoto.classList.add('cardWrap__photo-img');
-    friendsName.classList.add('mainInfo__friendsName');
-    friendAge.classList.add('mainInfo__age');
-    friendEmail.classList.add('moreWrap__email');
-    friendPhone.classList.add('moreWrap__phone');
-    friendCity.classList.add('mainInfo__city');
-    
-    friendsName.textContent = `${friend.name.first} ${friend.name.last}`;
-    friendAge.textContent = `Age: ${friend.dob.age}`;
-    friendEmail.textContent = `email: ${friend.email}`;
-    friendPhone.textContent = `phone: ${friend.phone}`;
-    friendCity.textContent = `City: ${friend.location.city}`;
-
-    friendPhoto.setAttribute('src', friend.picture.medium);
-    friendsName.setAttribute('href', `https://www.facebook.com/search/top?q=${friend.name.first}%20${friend.name.last}`);
-    friendsName.setAttribute('target', '_blank');
-    friendEmail.setAttribute('href', `mailto:${friend.email}`);
-    friendPhone.setAttribute('href', `tel:>${friend.phone}`);
-    friendCity.setAttribute('href', `https://www.google.com/maps/place/${friend.location.city}$`);
-    friendCity.setAttribute('target', '_blank');
-
-    moreWrap.append(friendPhone, friendEmail);
-    mainInfo.append(friendsName, friendAge, friendCity);
-    friendPhotoWrap.append(friendPhoto);
-    cardWrap.append(friendPhotoWrap, mainInfo, moreWrap);
-    FRIENDS_LIST_WRAP.append(cardWrap);
+    const FRIEND_CARD = document.createElement('div');
+    FRIEND_CARD.innerHTML =
+        `
+    <div class="cardWrap ${friend.gender === 'male' ? 'malegradient' : 'femalegradient'}">
+        <div class="cardWrap__photo-wrap">
+            <img
+                class="cardWrap__photo-img"
+                src= "${friend.picture.medium}"
+                alt="${friend.name.first} ${friend.name.last}"
+            />
+        </div>
+        <div class="mainInfo">
+            <a
+                class="mainInfo__friendsName"
+                href="https://www.facebook.com/search/top?q=${friend.name.first}%20${friend.name.last}"
+                target="_blank">
+                ${friend.name.first} ${friend.name.last}
+            </a>
+            <p class="mainInfo__age">
+                Age: ${friend.dob.age}
+            </p>
+            <a
+                class="mainInfo__city"
+                href="https://www.google.com/maps/place/${friend.location.city}$"
+                target="_blank">
+                    City: ${friend.location.city}
+            </a>
+        </div>
+        <div class="moreWrap">
+            <a class="moreWrap__phone" 
+                href="tel:>${friend.phone}">
+                phone: ${friend.phone}
+            </a>
+            <a class="moreWrap__email" 
+                href="mailto:${friend.email}">
+                email: ${friend.email}
+            </a>
+        </div>
+    </div>
+    `
+    FRIENDS_LIST_WRAP.append(FRIEND_CARD);
 }
 
-//find a friend by the name ------------------------------------
-SEARCH_FIELD.addEventListener('input', function() {
+SEARCH_FIELD.addEventListener('input', function () {
     let searchName = SEARCH_FIELD.value.toLowerCase();
     if (searchName !== '') {
-            searchResultList = friendsList.filter(function(friend) {
-            return friend.name.first.toLowerCase().includes(searchName) 
+        searchResultList = friendsList.filter(function (friend) {
+            return friend.name.first.toLowerCase().includes(searchName)
         })
         return renderFriendsList(searchResultList);
-    }  else {
+    } else {
+        searchResultList = [...friendsList]
         renderFriendsList(friendsList);
     }
 })
 
-//sorting List of friends --------------------------------------------
-SORTING_MENU.addEventListener('click', function(event) {
-    if (searchResultList) {
-        sortedList = searchResultList;
-    } else if (filteredByGenderList) {
+SORTING_MENU.addEventListener('click', function (event) {
+    if (filteredByGenderList) {
         sortedList = filteredByGenderList;
+    } else if (searchResultList) {
+        sortedList = searchResultList;
     } else {
         sortedList = [...friendsList];
     }
 
-    const sortByName = function(a, b) {
+    const searchByName = function (a, b) {
         const nameA = a.name.first.toUpperCase();
         const nameB = b.name.first.toUpperCase();
         return nameA < nameB ? -1 : nameA > nameB ? 1 : 0;
     }
 
-    const sortByAge = function(a, b) {
-        return (a.dob.age - b.dob.age);
+    const sortByAge = function (a, b) {
+        return a.dob.age - b.dob.age;
     }
-    
-    if (event.target === SORT_BY_NAME_AZ) {
-        sortedList.sort((a, b) => sortByName(a, b));
+
+    if (event.target.value === 'az') {
+        sortedList.sort(searchByName);
         renderFriendsList(sortedList);
 
-    } else if (event.target === SORT_BY_NAME_ZA) {
-        sortedList.sort((a, b) => sortByName(b, a));
+    } else if (event.target.value === 'za') {
+        sortedList.sort((a, b) => searchByName(b, a));
         renderFriendsList(sortedList);
 
-    } else if (event.target === SORT_BY_AGE_YO) {
-        sortedList.sort((a, b) => sortByAge(a, b));
+    } else if (event.target.value === 'young') {
+        sortedList.sort(sortByAge);
         renderFriendsList(sortedList);
 
-    } else if (event.target === SORT_BY_AGE_OY) {
+    } else if (event.target.value === 'older') {
         sortedList.sort((a, b) => sortByAge(b, a));
         renderFriendsList(sortedList);
-    } 
+    }
 })
 
-//filtering list of friends by gender
-FILTER_MENU.addEventListener('click', function(event) {
+FILTER_MENU.addEventListener('click', function (event) {
+
     if (searchResultList) {
         genderNeutralList = searchResultList;
     } else {
         genderNeutralList = [...friendsList];
     }
 
-    if (event.target === FILTER_BY_MALE) {
-        filteredByGenderList = genderNeutralList.filter(function(friend) {
-           return friend.gender === 'male'; 
-        })
-        renderFriendsList(filteredByGenderList);
-        
-    } else if (event.target === FILTER_BY_FEMALE) {
-        filteredByGenderList = genderNeutralList.filter(function(friend) {
-           return friend.gender === 'female'; 
+    if (event.target.value === 'male') {
+        filteredByGenderList = genderNeutralList.filter(function (friend) {
+            return friend.gender === 'male';
         })
         renderFriendsList(filteredByGenderList);
 
-    } else if (event.target === FILTER_BY_ALL) {
-        filteredByGenderList = [...friendsList];
+    } else if (event.target.value === 'female') {
+        filteredByGenderList = genderNeutralList.filter(function (friend) {
+            return friend.gender === 'female';
+        })
         renderFriendsList(filteredByGenderList);
-        }
-        
+
+    } else if (event.target.value === 'all') {
+        filteredByGenderList = genderNeutralList;
+        renderFriendsList(filteredByGenderList);
+    }
 })
 
-// showing user message when data loading error ------------------------------------------------------
 function renderErrorMessage() {
-    const messageErrorWrap =  document.createElement('div');
-    const messageError = document.createElement('p');
-    const tryMoreBtn = document.createElement('button');
-
-    messageErrorWrap.classList.add('error-message-wrap')
-    tryMoreBtn.classList.add('btn');
-    tryMoreBtn.classList.add('error-btn');
-    messageError.classList.add('error-msg-text');
-
-    messageError.textContent = 'OOPS! Something Bad Happened, please try again';
-    tryMoreBtn.textContent = 'Try Again';
-
-
-    messageErrorWrap.append(messageError, tryMoreBtn);
-    FRIENDS_LIST_WRAP.append(messageErrorWrap);
+    FRIENDS_LIST_WRAP.innerHTML =
+        `
+    <div class="error-message-wrap">
+        <p class="error-msg-text">OOPS! Something Bad Happened, please try again</p>
+        <button class="radio__button error-button" value="loading-error">Try Again</button>
+    </div>
+    `
 }
 
-// show/hide filter and sorting menus on mobile devices
-MOBILE_MENU_BUTTON.addEventListener('click', function() {
+MOBILE_MENU_BUTTON.addEventListener('click', function () {
     clickMenuBtn(MOBILE_MENU_BUTTON);
     clickMenuBtn(MENU_WRAP);
     scrollDepth = window.pageYOffset;
@@ -211,23 +227,21 @@ function clickMenuBtn(elem) {
     elem.classList.toggle('change');
 }
 
-//smooth scrolling to top
-function scrollToTop() {   
-    let timer = 0; 
+function scrollToTop() {
+    let timer = 0;
     if (scrollDepth > 100) {
         window.scrollTo(pageXOffset, scrollDepth);
         scrollDepth = scrollDepth - 60;
         timer = setTimeout(scrollToTop, 15);
     } else {
         clearTimeout(timer);
-        window.scrollTo(0,0);
+        window.scrollTo(0, 0);
     }
-   
-} 
 
-// when error - reSend Request to server -----------------------
-FRIENDS_LIST_WRAP.addEventListener('click', function(event) {
-    if (event.target.classList == 'btn error-btn') {
+}
+
+FRIENDS_LIST_WRAP.addEventListener('click', function (event) {
+    if (event.target.value == 'loading-error') {
         FRIENDS_LIST_WRAP.innerHTML = '';
         startApp();
     }
